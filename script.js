9ffc60520a295bba0f9d2d7501ea19743002cdc4
alysvolatile <alice.g.cooper@gmail.com>
2022-10-10 10:08:48 -0400

    remove comments

--- script.js
+++ script.js
@@ -14,14 +14,8 @@ function getQuotes(e) {
         https.onload = function() {
             if (this.status === 200) {
                 let data = JSON.parse(this.responseText)
-                let output = "";
                 shuffle(data)
-                // data.forEach((quote) => {
-                //     output += `
-                //     <li>"${quote.text}" - ${quote.author} </li>
-                //     <hr />
-                //     `
-                // })
+                let output = "";
                 for (let i = 0; i < data.length; i++) {
                     if (i == number) {break;}
                     output += `
@@ -60,19 +54,12 @@ function getRandomQuote(e) {
 
 // Fisher-Yates Shuffle
 function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle.
+    let currentIndex = array.length, randomIndex;
     while (currentIndex != 0) {
-  
-      // Pick a remaining element.
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
-      // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
-  
     return array;
   }
