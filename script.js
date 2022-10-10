de3bd4b65809e98ecb2152645294ea00f54fd70b
alysvolatile <alice.g.cooper@gmail.com>
2022-10-10 10:04:16 -0400

    ajax api functionality works, with chosing number of quotes

--- script.js
+++ script.js
@@ -8,28 +8,38 @@ randomButton.addEventListener('click', getRandomQuote)
 function getQuotes(e) {
     e.preventDefault();
     const number = document.querySelector("#number").value
-    const https = new XMLHttpRequest();
-    https.open("GET", "https://type.fit/api/quotes", true);
-    https.onload = function() {
-        if (this.status === 200) {
-            let data = JSON.parse(this.responseText)
-            let output = "";
-            data.forEach((quote) => {
-                output += `
-                <li>"${quote.text}" - ${quote.author} </li>
-                <hr />
-                `
-            })
-            document.querySelector(".quotes").innerHTML = output;
+    if (number && number > 0) {
+        const https = new XMLHttpRequest();
+        https.open("GET", "https://type.fit/api/quotes", true);
+        https.onload = function() {
+            if (this.status === 200) {
+                let data = JSON.parse(this.responseText)
+                let output = "";
+                // data.forEach((quote) => {
+                //     output += `
+                //     <li>"${quote.text}" - ${quote.author} </li>
+                //     <hr />
+                //     `
+                // })
+                for (let i = 0; i < data.length; i++) {
+                    if (i == number) {break;}
+                    output += `
+                        <li>"${data[i].text}" - ${data[i].author} </li>
+                        <hr />
+                        ` 
+                }
+                document.querySelector(".quotes").innerHTML = output;
+            }
         }
+        https.send();
+    } else {
+        return alert('Please enter a number.')
     }
-    https.send();
 }
 
 function getRandomQuote(e) {
     e.preventDefault();
     const randomNumber = Math.floor(Math.random() * 1643)
-    console.log(randomNumber)
     const https = new XMLHttpRequest();
     https.open("GET", "https://type.fit/api/quotes", true);
     https.onload = function() {
